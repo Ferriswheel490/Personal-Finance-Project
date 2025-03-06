e118a9ede68013dcbd52bbae1c7cb5498d981ea8
3334gFIOP-school <youremail@example.com>
2025-03-06 12:48:34 -0700

    eeeeee

--- essentials.py
+++ essentials.py
@@ -7,3 +7,12 @@ def end(message): # Ends the program with an end message
     cs()
     print(message)
     quit()
+
+def int_input(msg):
+    while True:
+        try:
+            output = int(input(msg))
+            break
+        except ValueError:
+            input("Invalid Input! (Only integers accepted)\nPress enter to continue")
+    return output
