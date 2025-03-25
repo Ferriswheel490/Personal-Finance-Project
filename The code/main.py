7af0ce406cf0b109f73f503aa477b779123d8e46
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-03-25 13:28:46 -0600

    all the repository

--- The code/main.py
+++ The code/main.py
@@ -1,1 +1,17 @@
 
+
+#this may be temporary
+def sign_in(username, password, user_database, account, make_account):
+    account =  input("do you have an account (y/n): ")
+    if account == "yes":
+        username_input = input("Username: ")
+        password_input = input("Password: ")
+        result = sign_in(username_input, password_input)
+        print(result)
+    if account == "no":
+        make_account = input("do you wanna make an account (y/n): ")
+        if make_account == "no":
+            return sign_in()
+        if make_account == "yes":
+            username_input = input("Username: ")
+            password_input = input("Password: ")
