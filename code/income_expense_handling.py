9d65e3715ed6ebe7b7ba9963a2ea0e7a8c48adc0
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-04-02 13:43:27 -0600

    tried to fix some thigns

--- code/income_expense_handling.py
+++ code/income_expense_handling.py
@@ -1,5 +1,4 @@
 #Cecily Strong income expense handling
-import datetime
 from account_handling import *
 def income_expense(dic):
     #display(dic) #for debugging
@@ -7,22 +6,19 @@ def income_expense(dic):
     if dic==False:
         return False
     else:
-        #helper functions
-        def latest(dic):
-            dic2={}
-            for x in list(dic.keys()):
-                dic2[x]=dic[x][-1]
-            return dic2
-        dic2=latest(dic)
         def tracking(amount,source,amount_input,source_input):
-            for x in list(dic2.keys()):
+            for x in list(dic.keys()):
                 if x == amount or x == source: #or x == 'name':
                     pass
                 elif x == 'total_funds':
                     if amount=='income_amount':
-                        dic['total_amount'].append(dic['total_amount'][-2]+float(amount))
+                        try:
+                            dic['total_funds'].append(dic['total_funds'][-1]+float(amount_input))
+                        except:
+                            dic['total_funds']=[0]
+                            dic['total_funds'].append(dic['total_funds'][-1]+float(amount_input))
                     elif amount=='expense_amount':
-                        dic['total_amount'].append(dic['total_amount'][-2]-float(amount))
+                        dic['total_funds'].append(dic['total_funds'][-1]-float(amount_input))
                 else:
                     dic[x].append('')
 
@@ -30,17 +26,22 @@ def income_expense(dic):
             dic[amount].append(amount_input) 
             dic[source].append(source_input) 
             return dic
+        def latest(dic):
+            dic2={}
+            for x in list(dic.keys()):
+                dic2[x]=dic[x][-1]
+            return dic
         
         #decision structure
         num=int(input("1. Track income\n2. Track expense\n3. Compare\n4. exit\n"))
         if num == 1: #track income
             tracking('income_amount','income_source',int(input('amount\n')),input('source\n'))
             new=latest(dic)
-            save(export('cecily'),new)
+            save(dic,new)
             return dic
         if num == 2: #track expenses
             tracking('expense_amount','expense_source',int(input('amount\n')),input('source\n')) 
-            save(export('cecily'),new)
+            save(dic,new)
             return dic
         if num == 3: #compare
             #year=int(input("what year do you want to see finances for?\n"))
@@ -54,17 +55,13 @@ def income_expense(dic):
             return dic
         if num == 4: #exit
             return dic
-        #''' 
         else: #stupid proofing
             print('invalid option') 
             income_expense(dic)
-        #'''
+    
 
 
-'''
-for x in dic['date']:
-    if dic['date'][dic['date'].index(x)][0:4] == str(year):
-        temp.append(x)
-print(temp) '''
+
+#debug()
 print(income_expense(load('a')))
-print(income_expense(load('ur mom')))
+print(income_expense(load('test')))
